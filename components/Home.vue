838c14380684efc5c84a12ea62d98796ef574050
Jakub Plesnik <jakub.plesnik@sde.cz>
2021-01-01 23:02:04 +0100

    Add home component 🏠

---
+++ components/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <section id="home" class="sections">
+    <div class="home-text" style="padding-top: 20%;">
+      <h1>Hi there! I'm <span class="jobs">a Developer.</span></h1>
+    </div>
+  </section>
+</template>
