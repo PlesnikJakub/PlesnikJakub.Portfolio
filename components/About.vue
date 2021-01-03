9871111d08f8d76cc38d6caf9fda7bdd41becc07
Jakub Plesnik <jakub.plesnik@sde.cz>
2021-01-03 20:58:27 +0100

    Update about

--- components/About.vue
+++ components/About.vue
@@ -2,28 +2,78 @@
   <section id="about-me">
     <MyParticles />
     <div id="content">
-      <h1 class="display-4 text-white">
-        Thanks.
-      </h1>
-      <div class="d-flex flex-wrap">
-        <a
-          href="https://drive.google.com/file/d/1j5BaNiTBU-DEwL9GmP_4OTNCjP6iGmE3/view?usp=sharing"
-          class="btn btn-light btn-lg mr-2"
-          target="_blank"
-          ><i class="fa fa-file"></i> View CV</a
-        >
-        <a
-          href="https://www.linkedin.com/in/rajeevsinghnaruka/"
-          target="_blank"
-          class="btn btn-dark btn-lg"
-          ><i class="fa fa-linkedin"></i> LinkedIn</a
-        >
+      <div class="col-lg-6 offset-3">
+        <div class="card shadow-lg">
+          <div class="card-body row">
+            <h2>Basic Info</h2>
+            <div class="basic-info row mb-5">
+              <BasicInfo title="Age" value="27" />
+              <BasicInfo
+                title="Curent Location"
+                value="Ostrava, Czech Republic"
+              />
+              <BasicInfo title="Alma mater" value="VSB TU-Ostrava" />
+              <BasicInfo title="Company" value="SDE Software solutions" />
+              <BasicInfo title="Company" value="SDE Software solutions" />
+              <BasicInfo title="Company" value="SDE Software solutions" />
+            </div>
+
+            <h2>Technologies</h2>
+            <p>
+              <i>
+                I do prefer to work on backend in .Net, but if project requires
+                it, I can work with different technologies on both frontend and
+                backend.
+              </i>
+            </p>
+            <div class="col-4 technologies">
+              <h5>Frontend</h5>
+              <ul>
+                <li>Vue js</li>
+                <li>React</li>
+                <li>Next</li>
+              </ul>
+            </div>
+            <div class="col-4 technologies">
+              <h5>Backend</h5>
+              <ul>
+                <li>C#</li>
+                <li>.Net</li>
+                <li>Php</li>
+                <li>Laravel</li>
+                <li>Symfony</li>
+              </ul>
+            </div>
+            <div class="col-4 technologies">
+              <h5>Data</h5>
+              <ul>
+                <li>R</li>
+                <li>Python</li>
+                <li>Graph theory</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <style scoped>
+.label {
+  display: inline-block;
+  width: 50px;
+}
+.technologies ul {
+  list-style: none;
+  list-style-type: "✅";
+}
+.card {
+  border-radius: 0px;
+  background: #212529AA;
+  color: white;
+  padding: 3rem;
+}
 #about-me {
   position: relative;
 }
@@ -31,7 +81,7 @@
   z-index: 1000;
   position: absolute;
   margin: auto;
-  top: 50%;
-  left: 250px;
+  top: 10%;
+  width: 100%;
 }
 </style>
---
+++ components/BasicInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="col-4 mb-3">
+    <h5>{{ title }}</h5>
+    <span>{{ value }}</span>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
