79e01b4e4794ac5baa636214e9c7d324bd52510e
PlesnikJakub <PlesnikJakub@users.noreply.github.com>
2021-04-05 10:07:25 +0000

    deploy: 104bf85ae3502a3136df9a876d6a5a35926a68bf

---
+++ _nuxt/static/1617617244/blog/hello2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FPortfolio\u002F_nuxt\u002Fstatic\u002F1617617244",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fhello2",config:{_app:{basePath:"\u002FPortfolio\u002F",assetsPath:"\u002FPortfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c9c7d250"}}}}(null));
